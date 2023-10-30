b9fbe7f27c56cd55bea80ce53414933054e7dbe0
Alvaro Peña <alvpe26@gmail.com>
2023-10-30 01:01:15 -0500

    Cleaning up the project

--- app/components/MyInput.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function MyInput() {
-  return (
-    <div>
-      <input type="text" className="border-2 border-gray-300 p-2 rounded-md" />
-    </div>
-  );
-}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import Navbar from './components/Navbar'
 
 export default function Page() {
   return (
     <div>
-      <p>Tantea</p>
+      <Navbar />
     </div>
   )
 }
